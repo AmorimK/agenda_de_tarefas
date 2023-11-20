6804b593bc11ed2277ec88328d407a74f5f7cce2
Kauã <kacjst05@gmail.com>
2023-11-20 20:13:16 -0300

    Update AnoInvalidoException.java

--- Agenda/src/AnoInvalidoException.java
+++ Agenda/src/AnoInvalidoException.java
@@ -1,5 +1,5 @@
 public class AnoInvalidoException extends Exception {
 	public AnoInvalidoException() {
-        super("Ano inv?lido. Insira um valor maior que 0.\n");
+        super("Ano inválido. Insira um valor maior que 0.\n");
     }
 }
