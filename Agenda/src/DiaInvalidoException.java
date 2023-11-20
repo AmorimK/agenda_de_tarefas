f1b9251d4a9c98ab5c5f4f7e4f0cf511e4d223ba
Kauã <kacjst05@gmail.com>
2023-11-20 20:14:02 -0300

    Update DiaInvalidoException.java

--- Agenda/src/DiaInvalidoException.java
+++ Agenda/src/DiaInvalidoException.java
@@ -1,5 +1,5 @@
 public class DiaInvalidoException extends Exception {
 	public DiaInvalidoException() {
-        super("Dia inv?lido. Insira um valor entre 1 e 28.\n");
+        super("Dia inválido. Insira um valor entre 1 e 28.\n");
     }
 }
